b71f098e97b6cb0106bcf95d9c3bd329a34c7bea
patrickcze <patrick.cze@gmail.com>
2017-03-08 12:11:08 -0700

    working on colour change working currently broken

--- A3/index.js
+++ A3/index.js
@@ -14,12 +14,29 @@ var messages = [];
 var users = {};
 var connections = [];
 
+var count = 0;
+
 // listen to 'chat' messages
 io.on('connection', function (socket) {
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length)
 
-    socket.emit('firstUserID', users.length + 1);
+    socket.emit('currentUsers', users);
+
+    count += 1;
+    var id = count;
+
+    users[count] = {
+        id: count,
+        nick: '' + count,
+        color: "#000000"
+    }
+
+    socket.emit('connectUserDetails', users[id]);
+
+    console.log(users);
+
+    io.emit('newUserConnected', users[id]);
 
     socket.emit('chatHistory', {
         chatHistory: messages
@@ -28,7 +45,7 @@ io.on('connection', function (socket) {
     //Handle disconnection
     socket.on('disconnect', function (data) {
         connections.splice(connections.indexOf(socket, 1));
-        console.log('Disconnected: %s sockets connected', connections.length)
+        console.log('Disconnected: %s sockets connected', connections.length);
     });
 
     socket.on('chat', function (msg) {
@@ -37,10 +54,66 @@ io.on('connection', function (socket) {
 
         msg.timeString = timeString;
 
-        messages.push(msg);
+        //Check for special test commands
+        // Check for colour change
+        if (msg.msg.indexOf('/nickcolor') >= 0) {
+            console.log('User is attempting to change nickcolour');
+            var tempuser = users[msg.user.id];
 
-        console.log(messages);
+            if (tempuser != null) {
+                var tempColor = '#' + msg.msg.replace('/nickcolor ', '').trim();
+                tempuser.color = tempColor;
 
-        io.emit('chat', msg);
+                users[msg.user.id] = tempuser;
+                console.log(users);
+            }
+            console.log(tempuser);
+            io.emit('updateUser', tempuser);
+        }
+        //Check for nickname change
+        else if (msg.msg.indexOf('nick') >= 0) {
+            messages.push(msg);
+            console.log(messages);
+            io.emit('chat', msg);
+        } else {
+            console.log('User is attempting to change nick');
+
+            console.log(checkForUniqueNick(msg));
+
+            var result = checkForUniqueNick(msg);
+            if (result.isUnique) {
+                //Username is unique need to update everyone
+                var tempuser = users[msg.user.id];
+
+                if (tempuser != null) {
+                    tempuser.nick = result.nickname;
+
+                    users[msg.user.id] = tempuser;
+
+                    console.log(users);
+                }
+                console.log(tempuser);
+                io.emit('updateUser', tempuser);
+            } else {
+                //Username is not unique need to pass error back to the client
+            }
+        }
     });
+
 });
+
+function checkForUniqueNick(messageObject) {
+    let unique = true;
+    let newNick = messageObject.msg.replace('/nick ', '').trim();
+
+    for (i in users) {
+        if (users[i].nick === newNick) {
+            unique = false;
+        }
+    }
+
+    return {
+        isUnique: unique,
+        nickname: newNick
+    };
+}
--- A3/public/client.js
+++ A3/public/client.js
@@ -2,31 +2,82 @@
 $(function () {
     var socket = io();
 
-    var clientID = "Nick";
-    var nickname = clientID;
+    var clientID = "";
+    var user;
+
+    var connectedUsers = {}
 
     $('form').submit(function () {
         socket.emit('chat', {
             msg: $('#m').val(),
-            nick: nickname
+            user: user
         });
         $('#m').val('');
         return false;
     });
 
     socket.on('chat', function (data) {
-        $('#messages').append($('<li>').text(data.timeString + "\t-\t" + data.nick + ": " + data.msg));
+        $('#messages').append($('<li class="msg-user-id-' + data.user.id + '">').text(data.timeString + "\t-\t" + data.user.nick + ": " + data.msg));
+
+        $('.msg-user-id-' + data.user.id).css({
+            "color": data.user.color
+        });
+
+        //bold the logged in user messages
+        $('.msg-user-id-' + user.id).css({
+            "font-weight": "bold"
+        });
     });
 
     socket.on('chatHistory', function (data) {
         for (i in data.chatHistory) {
             var msg = data.chatHistory[i]
-            $('#messages').append($('<li>').text(msg.timeString + "\t-\t" + msg.nick + ": " + msg.msg));
+            $('#messages').append($('<li>').text(msg.timeString + "\t-\t" + msg.user.nick + ": " + msg.msg));
         }
     });
 
-    socket.on('firstUserID', function (userid) {
-        clientID = userid;
-        nickname = clientID;
+    socket.on('connectUserDetails', function (data) {
+        console.log(data);
+        clientID = data.id;
+        user = data;
+
+        $('#usernameTitle').text("Your user name is: " + user.nick);
+
+        print($('.msg-user-id-' + data.id).text());
     });
+
+    socket.on('currentUsers', function (users) {
+        connectedUsers = users;
+
+        for (i in connectedUsers) {
+            console.log(connectedUsers[i]);
+            connectNewUser(connectedUsers[i]);
+        }
+    });
+
+    socket.on('newUserConnected', function (user) {
+        console.log('newUserConnected');
+        connectedUsers[user.id] = user;
+        console.log(connectedUsers);
+        connectNewUser(user);
+    });
+
+    socket.on('updateUser', function (data) {
+        connectedUsers[data.id] = data;
+        user = data;
+        print(connectedUsers);
+
+        $('#user-nick-id-' + data.id).text(data.nick);
+        $('#usernameTitle').text("Your user name is: " + data.nick);
+
+        print(user);
+    });
+
+    function connectNewUser(user) {
+        $('#onlineUsers').append($('<li id="user-nick-id-' + user.id + '">').text(user.nick));
+    }
 });
+
+function print(any) {
+    console.log("Local:", any);
+}
